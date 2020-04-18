5e197610a318e78881cfdeaad8122c5c4079a00b
Vincent Lee <vincentlee1776@gmail.com>
2020-04-17 23:15:10 -0400

    started rk4 setup should be similar for euler

--- rk4.m
+++ rk4.m
@@ -5,6 +5,15 @@
 % tspan: [t0 tf] interval
 % y0: [y01, y02] initial values
 % h: step size
-function [t,y] = rk4(f,tspan,y0,h)
 
+function [t,y] = rk4(f,tspan,y0,h)
+    current_time = tspan(1);
+    end_time = tspan(2);
+    iterations = (end_time - current_time) / h;
+    current_values = y0;
+    t = [];
+    y = [];
+    for i = 1:iterations
+        
+    end
 end

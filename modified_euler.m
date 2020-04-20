b661ec1c8811d48bb8aefcbf3f28d67c7ec0964c
Sean Trinh <strinh@stevens.edu>
2020-04-19 21:13:01 -0400

    adjusted modified euler input args

--- modified_euler.m
+++ modified_euler.m
@@ -1,17 +1,17 @@
 % Modified Euler method
-function approx = modified_euler(func, a, b, init1, init2, n)
+function approx = modified_euler(func, tspan, y0, n)
     approx = zeros(n+1,3);
-    h = (b - a) / n;
-    t = a;
+    h = (tspan(2) - tspan(1)) / n;
+    t = tspan(1);
     w = zeros(1, 2);
-    w(1) = init1;
-    w(2) = init2;
+    w(1) = y0(1);
+    w(2) = y0(2);
     approx(1, 1) = t;
     approx(1, 2) = w(1);
     approx(1, 3) = w(2);
     for i=1:n
         w = w + (h * (func(t, w) + func(t + h, w + (h * func(t, w)))) / 2);
-        t = a + (i * h);
+        t = tspan(1) + (i * h);
         approx(i+1, 1) = t;
         approx(i+1, 2) = w(1);
         approx(i+1, 3) = w(2);
--- modified_euler_model.m
+++ modified_euler_model.m
@@ -3,14 +3,12 @@
 %   Params:
 %       func - a system of two equations
 %           Ex: f = @(t,y) [(2-.5*y(2))*y(1); (-1+.5*y(1))*y(2)];
-%       a - the lower, inclusive bound for time t
-%       b - the upper, inclusive bound for time t
-%       initial_1 - the initial value for species 1
-%       initial_2 - the initial value for species 2
+%       tspan - [t0, tf]
+%       y0 - [y01, y02]
 %       n - the number of equally spaced approximations to produce on the
 %           interval [a,b] in addition to the approximation at t=0
-function modified_euler_model(func, a, b, initial_1, initial_2, n)
-    approx = modified_euler(func, a, b, initial_1, initial_2, n);
+function modified_euler_model(func, tspan, y0, n)
+    approx = modified_euler(func, tspan, y0, n);
     for i=1:length(approx)
         t = approx(i,1);
         y1 = approx(i,2);

6aceadff3b447a55e59f7fdfaaf047abacffa786
Sean Trinh <strinh@stevens.edu>
2020-04-19 20:58:23 -0400

    modified euler changes and model

--- modified_euler.m
+++ modified_euler.m
@@ -1,14 +1,18 @@
 % Modified Euler method
-function approx = modified_euler(func, a, b, initial_condition, n)
-    approx = zeros(n+1,2);
+function approx = modified_euler(func, a, b, init1, init2, n)
+    approx = zeros(n+1,3);
     h = (b - a) / n;
     t = a;
-    w = initial_condition;
+    w = zeros(1, 2);
+    w(1) = init1;
+    w(2) = init2;
     approx(1, 1) = t;
-    approx(1, 2) = w;
+    approx(1, 2) = w(1);
+    approx(1, 3) = w(2);
     for i=1:n
         w = w + (h * (func(t, w) + func(t + h, w + (h * func(t, w)))) / 2);
         t = a + (i * h);
         approx(i+1, 1) = t;
-        approx(i+1, 2) = w;
+        approx(i+1, 2) = w(1);
+        approx(i+1, 3) = w(2);
     end
---
+++ modified_euler_model.m
@@ -0,0 +1,21 @@
+%Function for modelling two species in a predator prey model using
+% modified euler's method
+%   Params:
+%       func - a system of two equations
+%           Ex: f = @(t,y) [(2-.5*y(2))*y(1); (-1+.5*y(1))*y(2)];
+%       a - the lower, inclusive bound for time t
+%       b - the upper, inclusive bound for time t
+%       initial_1 - the initial value for species 1
+%       initial_2 - the initial value for species 2
+%       n - the number of equally spaced approximations to produce on the
+%           interval [a,b] in addition to the approximation at t=0
+function modified_euler_model(func, a, b, initial_1, initial_2, n)
+    approx = modified_euler(func, a, b, initial_1, initial_2, n);
+    for i=1:length(approx)
+        t = approx(i,1);
+        y1 = approx(i,2);
+        y2 = approx(i,3);
+        plot(t, y1, 'b.', t, y2, 'r.'); hold on
+    end
+    hold off
+    legend('Species 1', 'Species 2')

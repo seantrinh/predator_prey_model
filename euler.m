4792c499b5790f179960f960f579ea7a55cd478f
Sean Trinh <strinh@stevens.edu>
2020-04-19 22:16:44 -0400

    modified output params for euler

--- euler.m
+++ euler.m
@@ -8,22 +8,23 @@
 %   Output:
 %       2D array of (t, w1, w2), where w is the approximation at t
 
-function approx = euler(func, tspan, y0, n)
-    approx = zeros(n+1, 3);
+function [time, approx] = euler(func, tspan, y0, n)
+    time = zeros(1, n+1);
+    approx = zeros(n+1, 2);
     h = (tspan(2) - tspan(1)) / n;
     t = tspan(1);
     w = zeros(1, 2);
     w(1) = y0(1);
     w(2) = y0(2);
-    approx(1, 1) = t;
-    approx(1, 2) = w(1);
-    approx(1, 3) = w(2);
+    time(1) = t;
+    approx(1, 1) = w(1);
+    approx(1, 2) = w(2);
     for i=1:n
         w = w + (h * func(t, w));
         t = tspan(1) + (i * h);
-        approx(i+1, 1) = t;
-        approx(i+1, 2) = w(1);
-        approx(i+1, 3) = w(2);
+        time(i+1) = t;
+        approx(i+1, 1) = w(1);
+        approx(i+1, 2) = w(2);
     end
     
     

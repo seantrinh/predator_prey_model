509fcf9e17bb4cd5cba8bcf530c02f276a93c4db
Sean Trinh <strinh@stevens.edu>
2020-04-20 13:46:29 -0400

    added phase plots for all models in main script

--- predator_prey.m
+++ predator_prey.m
@@ -17,8 +17,14 @@ function predator_prey
     xlabel("Time");
     ylabel("Population Size");
     
-    %Modified Euler
     figure(2)
+    plot(w_1(:,1), w_1(:,2));
+    title("Phase Plot Euler's Method");
+    xlabel("Prey");
+    ylabel("Predator");
+    
+    %Modified Euler
+    figure(3)
     [time_2, w_2] = modified_euler(f, tspan, y0, n);
     plot(time_2, w_2(:,1), 'b.', time_2, w_2(:,2), 'r.');
     legend('Species 1', 'Species 2');
@@ -26,8 +32,14 @@ function predator_prey
     xlabel("Time");
     ylabel("Population Size");
     
+    figure(4)
+    plot(w_2(:,1), w_2(:,2));
+    title("Phase Plot Modified Euler's Method");
+    xlabel("Prey");
+    ylabel("Predator");
+    
     %rk4
-    figure(3)
+    figure(5)
     [time_3, w_3] = rk4(f, tspan, y0, n);
     plot(time_3, w_3(:,1), 'b.', time_3, w_3(:,2), 'r.');
     legend('Species 1', 'Species 2');
@@ -35,8 +47,14 @@ function predator_prey
     xlabel("Time");
     ylabel("Population Size");
     
+    figure(6)
+    plot(w_3(:,1), w_3(:,2));
+    title("Phase Plot Runge Kutta 4th Order");
+    xlabel("Prey");
+    ylabel("Predator");
+    
     %rk45
-    figure(4)
+    figure(7)
     [time_4, w_4] = rk45(f, tspan, y0, tol, maxH, minH);
     plot(time_4, w_4(:,1), 'b.', time_4, w_4(:,2), 'r.');
     legend('Species 1', 'Species 2');
@@ -44,11 +62,23 @@ function predator_prey
     xlabel("Time");
     ylabel("Population Size");
     
+    figure(8)
+    plot(w_4(:,1), w_4(:,2));
+    title("Phase Plot Runge Kutta Fehlberg (rk45)");
+    xlabel("Prey");
+    ylabel("Predator");
+    
     %ode45 (built-in)
-    figure(5)
+    figure(9)
     [time_5, w_5] = ode45(f, tspan, y0);
     plot(time_5, w_5(:,1), 'b.', time_5, w_5(:,2), 'r.');
     legend('Species 1', 'Species 2');
     title("ODE 45 (Built-in)");
     xlabel("Time");
     ylabel("Population Size");
+    
+    figure(10)
+    plot(w_5(:,1), w_5(:,2));
+    title("Phase Plot ODE 45 (Built-in)");
+    xlabel("Prey");
+    ylabel("Predator");
